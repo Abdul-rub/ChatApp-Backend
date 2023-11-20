3a28543f6ec3c9d3ea13463d22ac71bda39f56f6
Abdul-rub <arub0419@gmail.com>
2023-11-20 15:52:04 +0530

    url-updated

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = app.listen(PORT, console.log(`Server started on ${PORT}`.yellow.b
 const io= require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin: 'http://localhost:5173',
+        origin: 'https://655b32475ad3637103190aee--voluble-crumble-2ce8bf.netlify.app',
     },
 })
 
